ee0e69e738bcaad8708a55d0d091fa72a682fc6b
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-11-15 23:07:12 +0200

    Create interface for CRUD operations

---
+++ model/src/main/java/com/college/model/database/interfaces/CRUDRepository.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.college.model.database.interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author Vagid Zibliuc
+ * @param <ID>
+ * @param <T>
+ */
+public interface CRUDRepository<ID,T> {
+    T getById(ID id);
+    List<T> getAll();
+    boolean save(T t);
+    boolean update(T t);
+    boolean delete(T t);
+    boolean deleteByID(ID id);
+}
