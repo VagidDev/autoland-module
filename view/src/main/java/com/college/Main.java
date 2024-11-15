136085ca94da17b743de1548322cabb69b0d4a47
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-11-15 20:22:25 +0200

    Add entry point for module

---
+++ view/src/main/java/com/college/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.college;
+
+import com.college.view.HomeWindow;
+
+/**
+ *
+ * @author Vagid Zibliuc
+ */
+public class Main {
+    public static void main(String[] args) {
+        new HomeWindow().showWindow();
+    }
+}
