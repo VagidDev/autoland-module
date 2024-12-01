c73ea41b027ebdb271037e2ea21e4cb67b5bc805
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-02 00:06:34 +0200

    Add controller for automobiles window

---
+++ controller/src/main/java/com/college/controller/AutomobileController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.college.controller;
+
+import com.college.model.Automobile;
+import com.college.model.database.interfaces.AutomobileDAO;
+import com.college.model.database.session.SessionManager;
+import java.util.List;
+
+/**
+ *
+ * @author Vagid Zibliuc
+ */
+public class AutomobileController {
+    private final AutomobileDAO automobileRepository;
+    
+    public AutomobileController() {
+        automobileRepository = SessionManager.getSession().getAutomobileRepository();
+    }
+    
+    public List<Automobile> getAllAutomobiles() {
+        return automobileRepository.getAll();
+    }
+    
+    public Automobile getAutoById(int id) {
+        return automobileRepository.getById(id);
+    }
+}
