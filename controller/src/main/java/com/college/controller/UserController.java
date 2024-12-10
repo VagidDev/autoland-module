b6f4144d9a3624d04181ed1be3d39e56b615686d
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-10 13:21:35 +0200

    Add methods for getting, saving and deleting user from database

--- controller/src/main/java/com/college/controller/UserController.java
+++ controller/src/main/java/com/college/controller/UserController.java
@@ -7,6 +7,9 @@ package com.college.controller;
 import com.college.model.User;
 import com.college.model.database.interfaces.UserDAO;
 import com.college.model.database.session.SessionManager;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,6 +18,7 @@ import java.util.List;
  */
 public class UserController {
     private final UserDAO userRepository;
+    private final static SimpleDateFormat smp = new SimpleDateFormat("yyyy-MM-dd");
         
     private User loginUser;
     
@@ -24,6 +28,10 @@ public class UserController {
        
     public List<User> getUsers() {
         return userRepository.getAll();
+    }
+    
+    public User getUserById(int id) {
+        return userRepository.getById(id);
     }
     
     public boolean authentificateUser(String login, String password) {
@@ -38,6 +46,36 @@ public class UserController {
     
     public User getAuthentificatedUser() {
         return loginUser;
+    }
+    
+    public boolean saveUser(String login, String password, String name, String surname, String birthday, String email, String telephone, String address) {
+        Date processedDate;
+        try {
+            processedDate = smp.parse(birthday);
+        } catch (ParseException ex) {
+            return false;
+        }
+        if (!email.endsWith("@gmail.com")) {
+            return false;
+        }
+        
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setBirthday(processedDate);
+        user.setEmail(email);
+        user.setTelephone(telephone);
+        user.setAddress(address);
+        
+        User newUser = userRepository.save(user);
+        
+        return newUser != null;
+    }
+    
+    public boolean deleteUserById(int id) {
+        return userRepository.deleteByID(id);
     }
     
 }
