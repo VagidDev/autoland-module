c838f864bcbf76ebc5063ade73f6ebc6d330a706
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-11 13:44:06 +0200

    Add new module, that provides new views to this project

---
+++ viewfx/src/main/java/com/college/viewfx/App.java
@@ -0,0 +1,26 @@
+package com.college.viewfx;
+
+import com.college.viewfx.view.*;
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+
+
+    @Override
+    public void start(Stage primaryStage) {
+//
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ viewfx/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.college.viewfx {
+    requires javafx.controls;
+    exports com.college.viewfx;
+
+//    uses java.sql.Driver;
+}
