1432477b3aa8389a2312a752f6c0672e4d59a88f
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-12 23:13:09 +0200

    Connect all add-forms to main form

--- viewfx/src/main/java/com/college/viewfx/view/AdminWindow.java
+++ viewfx/src/main/java/com/college/viewfx/view/AdminWindow.java
@@ -26,18 +26,21 @@ import javafx.stage.Stage;
 import javafx.geometry.Orientation;
 import javafx.geometry.Insets;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class AdminWindow {
-    private Stage primaryStage;
     private SplitPane splitPane;
+    private TableView tableView;
+    private Button addEquipButton;
     private final UserController userController;
     private final DealerController dealerController;
     private final WarrantyController warrantyController;
     private final AutomobileController automobileController;
     private final EquipmentController equipmentController;
     private final ContractController contractController;
-    //private Map<String, List<String>> classFields;
 
     public AdminWindow() {
         userController = ControllerManager.getUserController();
@@ -49,33 +52,27 @@ public class AdminWindow {
     }
 
     public void show() {
-        primaryStage = new Stage();
+        Stage primaryStage = new Stage();
         primaryStage.setTitle("Управление автомобилями");
 
-        // Создаем главный контейнер
         BorderPane root = new BorderPane();
 
-        // Создаем меню бар
         MenuBar menuBar = createMenuBar();
         root.setTop(menuBar);
 
-        // Создаем SplitPane для разделения кнопок и таблицы
         splitPane = new SplitPane();
         splitPane.setOrientation(Orientation.HORIZONTAL);
 
-        // Создаем панель с кнопками
         VBox buttonPanel = createButtonPanel();
 
-        // Создаем таблицу
-        TableView<User> table = createUserTable();
+        tableView = createUserTable();
 
-        // Добавляем панель с кнопками и таблицу в SplitPane
-        splitPane.getItems().addAll(buttonPanel, table);
-        splitPane.setDividerPositions(0.2); // Устанавливаем соотношение 30/70
+        splitPane.getItems().addAll(buttonPanel, tableView);
+        splitPane.setDividerPositions(0.2);
 
         root.setCenter(splitPane);
 
-        Scene scene = new Scene(root, 1300, 700); // Устанавливаем размер окна
+        Scene scene = new Scene(root, 1300, 700);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -102,8 +99,16 @@ public class AdminWindow {
         automobileItem.setOnAction(actionEvent -> {
             loadTable(Tables.AUTOMOBILE);
         });
+        MenuItem equipItem = new MenuItem("Комплектации");
+        equipItem.setOnAction(actionEvent -> {
+            loadTable(Tables.EQUIPMENT);
+        });
+        MenuItem contractItem = new MenuItem("Контракт");
+        contractItem.setOnAction(actionEvent -> {
+            loadTable(Tables.CONTRACT);
+        });
 
-        tableMenu.getItems().addAll(userItem, dealerItem, warrantyItem, automobileItem);
+        tableMenu.getItems().addAll(userItem, dealerItem, warrantyItem, automobileItem, equipItem, contractItem);
 
 
         menuBar.getMenus().addAll(fileMenu, tableMenu);
@@ -116,29 +121,45 @@ public class AdminWindow {
         buttonPanel.setPadding(new Insets(10));
 
         Button addButton = new Button("Добавить");
-        Button updateButton = new Button("Обновить");
+        addButton.setOnAction(actionEvent -> {
+            addValue();
+        });
+        Button updateButton = new Button("Редактировать");
+        updateButton.setOnAction(actionEvent -> {
+            updateValue();
+        });
         Button deleteButton = new Button("Удалить");
+        deleteButton.setOnAction(actionEvent -> {
+            deleteValue();
+        });
+        addEquipButton = new Button("Добавить комплектацию");
+        addEquipButton.setVisible(false);
+        addEquipButton.setOnAction(actionEvent -> {
+            addEquipValue();
+        });
 
-        // Устанавливаем максимальную ширину для кнопок
         addButton.setMaxWidth(Double.MAX_VALUE);
         updateButton.setMaxWidth(Double.MAX_VALUE);
         deleteButton.setMaxWidth(Double.MAX_VALUE);
+        addEquipButton.setMaxWidth(Double.MAX_VALUE);
 
-        buttonPanel.getChildren().addAll(addButton, updateButton, deleteButton);
+        buttonPanel.getChildren().addAll(addButton, updateButton, deleteButton, addEquipButton);
         return buttonPanel;
     }
 
     private void loadTable(Tables table) {
-        TableView tableView = null;
         switch (table) {
             case DEALER:
                 tableView = createDealerTable();
                 break;
             case AUTOMOBILE:
+                tableView = createAutomobileTable();
                 break;
             case CONTRACT:
+                tableView = createContractTable();
                 break;
             case EQUIPMENT:
+                tableView = createEquipmentTable();
                 break;
             case WARRANTY:
                 tableView = createWarrantyTable();
@@ -149,8 +170,16 @@ public class AdminWindow {
                 break;
         }
 
-
         splitPane.getItems().set(1, tableView);
+        checkEquipButton(table);
+    }
+
+    private void checkEquipButton(Tables table) {
+        if (table == Tables.AUTOMOBILE) {
+            addEquipButton.setVisible(true);
+        } else {
+            addEquipButton.setVisible(false);
+        }
     }
 
     private TableView<User> createUserTable() {
@@ -163,7 +192,7 @@ public class AdminWindow {
         loginColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getLogin()));
 
         TableColumn<User, String> passwordColumn = new TableColumn<>("Пароль");
-        passwordColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPassword()));
+        passwordColumn.setCellValueFactory(data -> new SimpleStringProperty("********"));
 
         TableColumn<User, String> nameColumn = new TableColumn<>("Имя");
         nameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getName()));
@@ -249,7 +278,6 @@ public class AdminWindow {
         return table;
     }
 
-
     private TableView<Automobile> createAutomobileTable() {
         TableView<Automobile> table = new TableView<>();
         table.getItems().clear();
@@ -284,4 +312,234 @@ public class AdminWindow {
 
         return table;
     }
+
+    private TableView<Equipment> createEquipmentTable() {
+        TableView<Equipment> table = new TableView<>();
+        table.getItems().clear();
+
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+
+        TableColumn<Equipment, String> autoColumn = new TableColumn<>("Автомобиль");
+        autoColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getAutomobile().getMark() + " " + data.getValue().getAutomobile().getModel()));
+
+        TableColumn<Equipment, String> nameColumn = new TableColumn<>("Название");
+        nameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getName()));
+
+        TableColumn<Equipment, String> engNameColumn = new TableColumn<>("Название двигателя");
+        engNameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEngineName()));
+
+        TableColumn<Equipment, String> engTypeColumn = new TableColumn<>("Тип двигателя");
+        engTypeColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEngineType()));
+
+        TableColumn<Equipment, String> engVolColumn = new TableColumn<>("Объем двигателя");
+        engVolColumn.setCellValueFactory(data -> new SimpleStringProperty(String.valueOf(data.getValue().getEngineVolume())));
+
+        TableColumn<Equipment, Number> horseColumn = new TableColumn<>("Лошадиные силы");
+        horseColumn.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getHorsepower()));
+
+        TableColumn<Equipment, String> suspensionColumn = new TableColumn<>("Тип подвески");
+        suspensionColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getSuspensiveType()));
+
+        TableColumn<Equipment, String> driveColumn = new TableColumn<>("Привод");
+        driveColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDriveType()));
+
+        TableColumn<Equipment, String> gearColumn = new TableColumn<>("КПП");
+        gearColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getGearboxType()));
+
+        TableColumn<Equipment, Number> countColumn = new TableColumn<>("Количество скоростей");
+        countColumn.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getSpeedCount()));
+
+        TableColumn<Equipment, String> fuelColumn = new TableColumn<>("Тип топлива");
+        fuelColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getFuelType()));
+
+        TableColumn<Equipment, String> interiorColumn = new TableColumn<>("Интерьер");
+        interiorColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getInterior()));
+
+        TableColumn<Equipment, String> bodyColumn = new TableColumn<>("Обвес");
+        bodyColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getBodyKit()));
+
+        TableColumn<Equipment, Number> weightColumn = new TableColumn<>("Вес");
+        weightColumn.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getWeigth()));
+
+        TableColumn<Equipment, Number> priceColumn = new TableColumn<>("Цена");
+        priceColumn.setCellValueFactory(data -> new SimpleDoubleProperty(data.getValue().getPrice()));
+
+        table.getColumns().addAll(autoColumn, nameColumn, engNameColumn, engTypeColumn, engVolColumn, horseColumn, suspensionColumn, driveColumn,
+                gearColumn, countColumn, fuelColumn, interiorColumn, bodyColumn, weightColumn, priceColumn);
+
+        List<Equipment> equipments = equipmentController.getAllEquipments();
+
+        ObservableList<Equipment> data = FXCollections.observableArrayList(equipments);
+
+        table.setItems(data);
+
+        return table;
+    }
+
+    private TableView<Contract> createContractTable() {
+        TableView<Contract> table = new TableView<>();
+        table.getItems().clear();
+
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+
+        TableColumn<Contract, String> userColumn = new TableColumn<>("Покупатель");
+        userColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getUser().getName() + " " + data.getValue().getUser().getSurname()));
+
+        TableColumn<Contract, String> dealerColumn = new TableColumn<>("Дилер");
+        dealerColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDealer().getName()));
+
+        TableColumn<Contract, String> autoColumn = new TableColumn<>("Автомобиль");
+        autoColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getAutomobile().getMark() + " " + data.getValue().getAutomobile().getModel()));
+
+        TableColumn<Contract, String> equipColumn = new TableColumn<>("Комплектация");
+        equipColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEquipment().getName()));
+
+        TableColumn<Contract, String> warrantyColumn = new TableColumn<>("Гарантия");
+        warrantyColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getWarranty().getName()));
+
+        TableColumn<Contract, String> dataColumn = new TableColumn<>("Дата покупки");
+        dataColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getConclusionDate().toString()));
+
+        table.getColumns().addAll(userColumn, dealerColumn, equipColumn, warrantyColumn, dataColumn);
+
+        List<Contract> contracts = contractController.getAllContracts();
+
+        ObservableList<Contract> data = FXCollections.observableArrayList(contracts);
+
+        table.setItems(data);
+
+        return table;
+    }
+
+    private void addValue() {
+        String form = tableView.getItems().get(0).getClass().getSimpleName();
+        switch (form) {
+            case "User":
+                new UserForm(userController, this::refreshTable).show();
+                break;
+            case "Dealer":
+                new DealerForm(dealerController, this::refreshTable).show();
+                break;
+            case "Warranty":
+                new WarrantyForm(warrantyController, this::refreshTable).show();
+                break;
+            case "Automobile":
+                new AutomobileForm(automobileController, this::refreshTable).show();
+                break;
+            default:
+                new Alert(Alert.AlertType.WARNING, "В данной таблице нельзя добавлять информацию", ButtonType.APPLY).showAndWait();
+                break;
+        }
+    }
+
+    private void addEquipValue() {
+        Automobile automobile = (Automobile) getSelectedItem();
+        if (automobile == null) {
+            new Alert(Alert.AlertType.WARNING, "Выберите автомобиль!", ButtonType.OK).show();
+            return;
+        }
+        new EquipmentForm(automobile, equipmentController, this::refreshTable).show();
+        loadTable(Tables.EQUIPMENT);
+    }
+
+    private void updateValue() {
+        Object obj = getSelectedItem();
+        if (obj == null) {
+            new Alert(Alert.AlertType.WARNING, "Выберите строку!", ButtonType.APPLY).showAndWait();
+            return;
+        }
+        String form = obj.getClass().getSimpleName();
+        switch (form) {
+            case "Dealer":
+                new DealerForm((Dealer) obj, dealerController, this::refreshTable).show();
+                break;
+            case "Warranty":
+                new WarrantyForm((Warranty) obj, warrantyController, this::refreshTable).show();
+                break;
+            case "Automobile":
+                new AutomobileForm((Automobile) obj, automobileController, this::refreshTable).show();
+                break;
+            case "Equipment":
+                new EquipmentForm((Equipment) obj, equipmentController, this::refreshTable).show();
+                break;
+            default:
+                new Alert(Alert.AlertType.WARNING, "В данной таблице нельзя изменять информацию", ButtonType.APPLY).showAndWait();
+                break;
+        }
+    }
+
+    private void deleteValue() {
+        Object obj = getSelectedItem();
+        String form = obj.getClass().getSimpleName();
+        var alert = new Alert(Alert.AlertType.CONFIRMATION, "Вы уверенны, что хотите удалить данную строку?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+        if (ButtonType.NO == alert.getResult())
+            return;
+
+        boolean isProcessed = false;
+
+        switch (form) {
+            case "Dealer":
+                isProcessed = dealerController.deleteDealer(((Dealer) obj).getId());
+                break;
+            case "User":
+                isProcessed = userController.deleteUserById(((User) obj).getId());
+                break;
+            case "Warranty":
+                isProcessed = warrantyController.deleteWarranty(((Warranty) obj).getId());
+                break;
+            case "Automobile":
+                isProcessed = automobileController.deleteAutomobile(((Automobile) obj).getId());
+                break;
+            case "Equipment":
+                Equipment equipment = ((Equipment) obj);
+                isProcessed = equipmentController.deleteEquipment(equipment.getAutomobile().getId(), equipment.getId());
+                break;
+        }
+        if (isProcessed) {
+            new Alert(Alert.AlertType.INFORMATION, "Строка удалена", ButtonType.APPLY).showAndWait();
+            refreshTable();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Нельзя удалить данную запись так как она используется в другой таблице!", ButtonType.APPLY).showAndWait();
+        }
+
+    }
+
+    public void refreshTable() {
+        try {
+            String form = tableView.getItems().get(0).getClass().getSimpleName();
+            List list = null;
+            switch (form) {
+                case "User":
+                    list = userController.getUsers();
+                    break;
+                case "Dealer":
+                    list = dealerController.getAllDealers();
+                    break;
+                case "Warranty":
+                    list = warrantyController.getAllWarranties();
+                    break;
+                case "Automobile":
+                    list = automobileController.getAllAutomobiles();
+                    break;
+                case "Equipment":
+                    list = equipmentController.getAllEquipments();
+                    break;
+                case "Contract":
+                    list = contractController.getAllContracts();
+                    break;
+                default:
+                    list = new ArrayList();
+                    break;
+            }
+            tableView.getItems().setAll(list);
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    private Object getSelectedItem() {
+        return tableView.getSelectionModel().getSelectedItem();
+    }
+
 }
