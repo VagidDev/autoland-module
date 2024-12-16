357503ed60ebe0e2d084420bc50d3187da4906d2
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-16 08:48:23 +0200

    Add DriveType entity and it's dao with implementation

--- model/src/main/java/com/college/model/database/implementations/DriveTypeImpl.java
+++ model/src/main/java/com/college/model/database/implementations/DriveTypeImpl.java
@@ -1,33 +1,33 @@
 package com.college.model.database.implementations;
 
-import com.college.model.BodyType;
+import com.college.model.DriveType;
 import com.college.model.database.Database;
 import com.college.model.database.exceptions.CascadeDependencyException;
 import com.college.model.database.exceptions.EntityNotFoundException;
-import com.college.model.database.interfaces.BodyTypeDAO;
+import com.college.model.database.interfaces.DriveTypeDAO;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BodyTypeImpl implements BodyTypeDAO {
-    private static final String SELECT_SQL = "SELECT * FROM au_body_type";
-    private static final String SELECT_BY_ID_SQL = "SELECT * FROM au_body_type WHERE bt_id = ?";
-    private static final String INSERT_VALUES_SQL = "INSERT INTO au_body_type (bt_name) VALUES (?)";
-    private static final String UPDATE_VALUES_SQL = "UPDATE au_body_type SET bt_name = ? WHERE bt_id = ?";
-    private static final String DELETE_SQL = "DELETE FROM au_body_type WHERE bt_id = ?";
+public class DriveTypeImpl implements DriveTypeDAO {
+    private static final String SELECT_SQL = "SELECT * FROM au_drive_type";
+    private static final String SELECT_BY_ID_SQL = "SELECT * FROM au_drive_type WHERE dt_id = ?";
+    private static final String INSERT_VALUES_SQL = "INSERT INTO au_drive_type (dt_name) VALUES (?)";
+    private static final String UPDATE_VALUES_SQL = "UPDATE au_drive_type SET dt_name = ? WHERE dt_id = ?";
+    private static final String DELETE_SQL = "DELETE FROM au_drive_type WHERE dt_id = ?";
 
     @Override
-    public BodyType getById(Integer integer) {
+    public DriveType getById(Integer integer) {
         try (Connection connection = Database.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID_SQL);
             statement.setInt(1, integer);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                BodyType bodyType = new BodyType();
-                bodyType.setId(integer);
-                bodyType.setName(resultSet.getString("bt_name"));
-                return bodyType;
+                DriveType driveType = new DriveType();
+                driveType.setId(resultSet.getInt("dt_id"));
+                driveType.setName(resultSet.getString("dt_name"));
+                return driveType;
             }
             return null;
         } catch (SQLException ex) {
@@ -36,36 +36,34 @@ public class BodyTypeImpl implements BodyTypeDAO {
     }
 
     @Override
-    public List<BodyType> getAll() {
+    public List<DriveType> getAll() {
         try (Connection connection = Database.getConnection()) {
-            List<BodyType> bodyTypes = new ArrayList<>();
+            List<DriveType> driveTypes = new ArrayList<>();
             Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(SELECT_SQL);
-            while (result.next()) {
-                BodyType bodyType = new BodyType();
-                bodyType.setId(result.getInt("bt_id"));
-                bodyType.setName(result.getString("bt_name"));
-                bodyTypes.add(bodyType);
+            ResultSet resultSet = statement.executeQuery(SELECT_SQL);
+            while (resultSet.next()) {
+                DriveType driveType = new DriveType();
+                driveType.setId(resultSet.getInt("dt_id"));
+                driveType.setName(resultSet.getString("dt_name"));
+                driveTypes.add(driveType);
             }
-            return bodyTypes;
+            return driveTypes;
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
     }
 
     @Override
-    public BodyType save(BodyType bodyType) {
+    public DriveType save(DriveType driveType) {
         try (Connection connection = Database.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(INSERT_VALUES_SQL, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, bodyType.getName());
-
+            statement.setString(1, driveType.getName());
             statement.execute();
 
-            ResultSet result = statement.getGeneratedKeys();
-
-            if (result.next()) {
-                bodyType.setId(result.getInt(1));
-                return bodyType;
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                driveType.setId(resultSet.getInt(1));
+                return driveType;
             }
             return null;
         } catch (SQLException ex) {
@@ -74,14 +72,15 @@ public class BodyTypeImpl implements BodyTypeDAO {
     }
 
     @Override
-    public void update(BodyType bodyType) {
+    public void update(DriveType driveType) {
         try (Connection connection = Database.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(UPDATE_VALUES_SQL);
-            statement.setString(1, bodyType.getName());
-            statement.setInt(2, bodyType.getId());
+            statement.setString(1, driveType.getName());
+            statement.setInt(2, driveType.getId());
             int rowCount = statement.executeUpdate();
+
             if (rowCount == 0) {
-                throw new EntityNotFoundException("BodyType with id=" + bodyType.getId() + " did not update, because it does not exists!");
+                throw new EntityNotFoundException("Drive type with id=" + driveType.getId() + " did not update, because it does not exists!");
             }
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
@@ -89,13 +88,13 @@ public class BodyTypeImpl implements BodyTypeDAO {
     }
 
     @Override
-    public void delete(BodyType bodyType) throws CascadeDependencyException {
+    public void delete(DriveType driveType) throws CascadeDependencyException {
         try (Connection connection = Database.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(DELETE_SQL);
-            statement.setInt(1, bodyType.getId());
+            statement.setInt(1, driveType.getId());
             statement.executeUpdate();
         } catch (SQLIntegrityConstraintViolationException ex) {
-            throw new CascadeDependencyException("BodyType with id=" + bodyType.getId() + " using in other tables");
+            throw new CascadeDependencyException("DriveType with id=" + driveType.getId() + " using in other tables");
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
@@ -108,7 +107,7 @@ public class BodyTypeImpl implements BodyTypeDAO {
             statement.setInt(1, id);
             statement.executeUpdate();
         } catch (SQLIntegrityConstraintViolationException ex) {
-            throw new CascadeDependencyException("BodyType with id=" + id + " using in other tables");
+            throw new CascadeDependencyException("DriveType with id=" + id + " using in other tables");
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
