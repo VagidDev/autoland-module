9a526a523f1cc4a1a844246f0107f5196983ba4a
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-16 17:00:42 +0200

    Configure session and session-manager

--- model/src/main/java/com/college/model/database/session/Session.java
+++ model/src/main/java/com/college/model/database/session/Session.java
@@ -1,31 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.college.model.database.session;
 
 import com.college.model.database.implementations.*;
 import com.college.model.database.interfaces.*;
 
 /**
- *
  * @author Vagid Zibliuc
  */
+
 public class Session {
 
     private final UserDAO userRepository;
     private final DealerDAO dealerRepository;
     private final WarrantyDAO warrantyRepository;
+    private final BodyTypeDAO bodyTypeRepository;
     private final AutomobileDAO automobileRepository;
+    private final DriveTypeDAO driveTypeRepository;
+    private final FuelTypeDAO fuelTypeRepository;
+    private final EngineTypeDAO engineTypeRepository;
+    private final GearboxTypeDAO gearboxTypeRepository;
+    private final SuspensionTypeDAO suspensionTypeRepository;
     private final EquipmentDAO equipmentRepository;
     private final ContractDAO contractRepository;
+
 
     public Session() {
         this.userRepository = new UserImpl();
         this.dealerRepository = new DealerImpl();
         this.warrantyRepository = new WarrantyImpl();
-        this.automobileRepository = new AutomobileImpl();
-        this.equipmentRepository = new EquipmentImpl(automobileRepository);
+        this.bodyTypeRepository = new BodyTypeImpl();
+        this.automobileRepository = new AutomobileImpl(bodyTypeRepository);
+        this.driveTypeRepository = new DriveTypeImpl();
+        this.fuelTypeRepository = new FuelTypeImpl();
+        this.engineTypeRepository = new EngineTypeImpl();
+        this.gearboxTypeRepository = new GearboxTypeImpl();
+        this.suspensionTypeRepository = new SuspensionTypeImpl();
+        this.equipmentRepository = new EquipmentImpl(automobileRepository, engineTypeRepository, suspensionTypeRepository, driveTypeRepository, gearboxTypeRepository, fuelTypeRepository);
         this.contractRepository = new ContractImpl(userRepository, dealerRepository, warrantyRepository, automobileRepository, equipmentRepository);
     }
 
@@ -52,5 +61,28 @@ public class Session {
     public ContractDAO getContractRepository() {
         return contractRepository;
     }
-    
+
+    public BodyTypeDAO getBodyTypeRepository() {
+        return bodyTypeRepository;
+    }
+
+    public DriveTypeDAO getDriveTypeRepository() {
+        return driveTypeRepository;
+    }
+
+    public FuelTypeDAO getFuelTypeRepository() {
+        return fuelTypeRepository;
+    }
+
+    public EngineTypeDAO getEngineTypeRepository() {
+        return engineTypeRepository;
+    }
+
+    public GearboxTypeDAO getGearboxTypeRepository() {
+        return gearboxTypeRepository;
+    }
+
+    public SuspensionTypeDAO getSuspensionTypeRepository() {
+        return suspensionTypeRepository;
+    }
 }
--- model/src/main/java/com/college/model/database/session/SessionManager.java
+++ model/src/main/java/com/college/model/database/session/SessionManager.java
@@ -1,18 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.college.model.database.session;
 
 /**
- *
  * @author Vagid Zibliuc
  */
+
 public final class SessionManager {
     private static final Session session = new Session();
+
     public static Session getSession() {
         return session;
     }
-    
-    private SessionManager(){}
+
+    private SessionManager() {}
 }
