d13f62fd5682e9f2cabb53f15df85f1ac5d352e7
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-12-17 21:21:34 +0200

    Export models, and their DAO's

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -1,5 +1,11 @@
 module com.college.model {
     requires java.sql;
 
+    exports com.college.model;
+    exports com.college.model.database.session;
+    exports com.college.model.database.interfaces;
+    exports com.college.model.database.exceptions;
+    exports com.college.model.keys;
+
     uses java.sql.Driver;
 }
