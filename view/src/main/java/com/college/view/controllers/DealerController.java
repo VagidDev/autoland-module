03a6a93b29d925f32c3a28220fad384b925d5e9c
Zibliuc Vagid <vagidzibluk@gmail.com>
2025-01-16 17:42:02 +0200

    Added ability to click and select dealer-panes, reviewed css-code

--- view/src/main/java/com/college/view/controllers/DealerController.java
+++ view/src/main/java/com/college/view/controllers/DealerController.java
@@ -9,10 +9,10 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 import java.io.IOException;
@@ -53,28 +53,41 @@ public class DealerController {
         nameLabel.setLayoutX(14);
         nameLabel.setLayoutY(14);
         nameLabel.setPrefSize(165, 34);
-        nameLabel.setTextFill(Color.WHITE);
         nameLabel.setFont(Font.font("Lucida Bright Demibold", 18));
+        nameLabel.getStyleClass().add("main-text");
 
         Label addressLabel = new Label(address);
         addressLabel.setLayoutX(14);
         addressLabel.setLayoutY(56);
         addressLabel.setPrefSize(165, 34);
-        addressLabel.setTextFill(Color.WHITE);
         addressLabel.setFont(Font.font("Lucida Bright Demibold", 16));
+        addressLabel.getStyleClass().add("main-text");
 
         Label phoneLabel = new Label(phoneNumber);
         phoneLabel.setLayoutX(14);
         phoneLabel.setLayoutY(101);
         phoneLabel.setPrefSize(165, 34);
-        phoneLabel.setTextFill(Color.web("#909090"));
         phoneLabel.setFont(Font.font("Lucida Bright Demibold", 14));
+        phoneLabel.getStyleClass().add("telephone-text");
 
         pane.getChildren().addAll(nameLabel, addressLabel, phoneLabel);
 
+        pane.setOnMouseClicked(this::onDealerPaneClicked);
 
         return pane;
     }
+
+    private void onDealerPaneClicked(MouseEvent mouseEvent) {
+        Object clickedObject = mouseEvent.getSource();
+        if (clickedObject instanceof Pane clikedPane) {
+            dealerList.getChildren().stream()
+                    .filter(node -> node.getStyleClass().getLast().equals("clicked-list-element"))
+                    .forEach(node -> node.getStyleClass().remove("clicked-list-element"));
+
+            clikedPane.getStyleClass().add("clicked-list-element");
+        }
+    }
+
 
     @FXML
     public void onClickBackButton(ActionEvent event) throws IOException {
