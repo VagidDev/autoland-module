8be854f01f8fb6c135b44cea46b89f13849732cf
Zibliuc Vagid <vagidzibluk@gmail.com>
2025-02-09 22:30:45 +0200

    Add simple tests for validate-method

--- controller/src/test/java/com/college/controller/UserControllerTest.java
+++ controller/src/test/java/com/college/controller/UserControllerTest.java
@@ -1,8 +1,8 @@
 package com.college.controller;
 
+import com.college.controller.validators.UserValidationResponse;
 import com.college.model.User;
 import com.college.model.database.interfaces.UserDAO;
-import com.college.model.database.session.SessionManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,14 +21,11 @@ class UserControllerTest {
     private UserDAO userDAO;
     @InjectMocks
     private UserController userController;
+    private User user;
 
-
-    @Test
-    void shouldReturnUser() {
-        Mockito.when(userDAO.save(Mockito.any(User.class))).thenReturn(new User(25, "qwerty123", "qwerty123", "USER",
-                "John", "Doe", new Date(), "john@gmail.com", "+37367292196", "some address"));
-
-        User user = new User();
+    @BeforeEach
+    void setUp() {
+        user = new User();
         user.setLogin("qwerty123");
         user.setPassword("qwerty123");
         user.setName("John");
@@ -37,6 +34,74 @@ class UserControllerTest {
         user.setBirthday(new Date());
         user.setAddress("some address");
         user.setTelephone("+37367292196");
+    }
+
+    @Test
+    void validateUser_shouldBeValid() {
+        UserValidationResponse response = userController.validateUser(user);
+        assertEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidLogin() {
+        user.setLogin("qwerty()grg");
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidPassword() {
+        user.setPassword("******");
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidName() {
+        user.setName(null);
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidSurname() {
+        user.setSurname("          ");
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidBirthday() {
+        user.setBirthday(new Date());
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidEmail() {
+        user.setEmail("null_email");
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidTelephone() {
+        user.setTelephone("695216484513");
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void validateUser_shouldNotBeValid_InvalidAddress() {
+        user.setAddress("some address");
+        UserValidationResponse response = userController.validateUser(user);
+        assertNotEquals(UserValidationResponse.VALID, response);
+    }
+
+    @Test
+    void createUser_shouldCreate() {
+        Mockito.when(userDAO.save(Mockito.any(User.class))).thenReturn(new User(25, "qwerty123", "qwerty123", "USER",
+                "John", "Doe", new Date(), "john@gmail.com", "+37367292196", "some address"));
 
         User responseUser = userController.createUser(user);
 
@@ -44,19 +109,11 @@ class UserControllerTest {
     }
 
     @Test
-    void shouldNotReturnUserBecauseOfInvalidLogin() {
+    void createUser_shouldNotCreate_InvalidLogin() {
         Mockito.lenient().when(userDAO.save(Mockito.any(User.class))).thenReturn(new User(25, "qwerty123", "qwerty123", "USER",
                 "John", "Doe", new Date(), "john@gmail.com", "+37367292196", "some address"));
 
-        User user = new User();
         user.setLogin("qwer!!!!!+-ty123");
-        user.setPassword("qwerty123");
-        user.setName("John");
-        user.setSurname("Doe");
-        user.setEmail("john@gmail.com");
-        user.setBirthday(new Date());
-        user.setAddress("some address");
-        user.setTelephone("+37367292196");
 
         User responseUser = userController.createUser(user);
 
