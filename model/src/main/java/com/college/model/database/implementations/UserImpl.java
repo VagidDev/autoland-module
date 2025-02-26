7b6a8e6193ca078d89d7544f799dd72e7d39eb6f
Zibliuc Vagid <vagidzibluk@gmail.com>
2025-02-26 21:50:35 +0200

    Optimize methods in User Repository (UserImpl)

--- model/src/main/java/com/college/model/database/implementations/UserImpl.java
+++ model/src/main/java/com/college/model/database/implementations/UserImpl.java
@@ -19,105 +19,28 @@ import java.util.List;
  *
  * @author Vagid Zibliuc
  */
-public class UserImpl implements UserDAO {
+public class UserImpl extends AbstractCRUDRepository<Integer, User> implements UserDAO {
 
-    private static final String GET_BY_ID_QUERY = "SELECT * FROM au_users WHERE u_id = ?";
-    private static final String GET_ALL_QUERY = "SELECT * FROM au_users";
-    private static final String INSERT_QUERY = "INSERT INTO au_users (u_login, u_password, u_name, u_surname, u_birthday, u_email, u_telephone, u_address) "
-                                                + "VALUES(?,?,?,?,?,?,?,?)";
-    private static final String UPDATE_QUERY = "UPDATE au_users\n"
-                                                + "SET u_login = ?, u_password = ?, u_name = ?, u_surname = ?, u_birthday = ?, u_email = ?, u_telephone = ?, u_address = ?, u_avatar = ?\n"
-                                                + "WHERE u_id = ?;";
-    private static final String DELETE_QUERY = "DELETE FROM au_users WHERE u_id = ?;";
-    private static final String SEARCH_BY_LOGIN_AND_PASSWORD = "SELECT * FROM au_users WHERE u_login = ? AND u_password = ?";
-    private static final String USER_EXISTS_QUERY = "SELECT * FROM au_users WHERE u_login = ?";
     //Hibernate queries
-    private static final String GET_BY_ID_HQL = "FROM User WHERE id = :userid";
+    private static final String GET_BY_ID_HQL = "FROM User WHERE id = :id";
     private static final String GET_ALL_HQL = "FROM User";
     private static final String SEARCH_BY_LOGIN_AND_PASSWORD_HQL = "FROM User WHERE login = :login AND password = :password";
     private static final String USER_EXISTS_HQL = "FROM User WHERE login = :login";
     
+
     @Override
-    public User getById(Integer id) {
-        try (Session session = SessionManager.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Query<User> getQuery = session.createQuery(GET_BY_ID_HQL, User.class);
-            getQuery.setParameter("userid", id);
-            User user = getQuery.stream().findFirst().orElse(null);
-            transaction.commit();
-            return user;
-        }
+    protected Class<User> getEntityClass() {
+        return User.class;
     }
 
     @Override
-    public List<User> getAll() {
-        try (Session session = SessionManager.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Query<User> getAllQuery = session.createQuery(GET_ALL_HQL, User.class);
-            List<User> users = getAllQuery.getResultList();
-            transaction.commit();
-            return users;
-        }
+    protected String getByIdQuery() {
+        return GET_BY_ID_HQL;
     }
 
     @Override
-    public User save(User t) {
-        Session session = SessionManager.getSessionFactory().openSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            session.persist(t);
-            transaction.commit();
-            return t;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            return null;
-        } finally {
-            session.close();
-        }
-    }
-
-    @Override
-    public void update(User t) {
-        Session session = SessionManager.getSessionFactory().openSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            session.merge(t);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            session.close();
-        }
-    }
-
-    @Override
-    public void delete(User t) throws CascadeDependencyException {
-        Session session = SessionManager.getSessionFactory().openSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            session.remove(t);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new CascadeDependencyException("Cannot delete user with id=" + t.getId() + ", because it is using in other table");
-        } finally {
-            session.close();
-        }
-    }
-
-    @Override
-    public void deleteByID(Integer id) throws CascadeDependencyException {
-        User userToDelete = getById(id);
-        delete(userToDelete);
+    protected String getAllQuery() {
+        return GET_ALL_HQL;
     }
 
     @Override
