4e215586946ce9aedac6e51cd9de0dcf21e602d1
Zibliuc Vagid <vagidzibluk@gmail.com>
2025-03-22 23:15:26 +0200

    Add reading from database for account form

--- view/src/main/java/com/college/view/controllers/AccountController.java
+++ view/src/main/java/com/college/view/controllers/AccountController.java
@@ -1,10 +1,14 @@
 package com.college.view.controllers;
 
+import com.college.controller.AuthorizationController;
+import com.college.model.entity.User;
+import com.college.view.core.ControllerManager;
 import com.college.view.core.StageService;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -13,8 +17,23 @@ import javafx.stage.Stage;
 public class AccountController {
     @FXML
     private ImageView avatar;
+    @FXML
+    private Label fullName;
+    @FXML
+    private Label login;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label email;
+    @FXML
+    private Label address;
+
+    private AuthorizationController authorizationController;
 
     public void initialize() {
+        authorizationController = ControllerManager.getAuthorizationController();
+        setUserInfo();
+
         Circle circle = new Circle(100, 100, 100);
         avatar.setClip(circle);
 
@@ -23,6 +42,16 @@ public class AccountController {
             stage.setOnCloseRequest(event -> StageService.buildAndShowStage("Home", "home-form.fxml"));
         });
 
+    }
+
+    private void setUserInfo() {
+        User user = authorizationController.getCurrentUser();
+
+        fullName.setText(user.getName() + " " + user.getSurname());
+        login.setText(user.getLogin());
+        phone.setText(user.getTelephone());
+        email.setText(user.getEmail());
+        address.setText(user.getAddress());
     }
 
     public void editButtonClicked(ActionEvent actionEvent) {
